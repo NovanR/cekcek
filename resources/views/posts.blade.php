9c244dc3acb3c992fb72067b356714adcce39a36
Novan <novanramdan7@gmail.com>
2024-11-04 18:50:51 +0700

    Video 13 - Database Seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,7 +11,7 @@
                 <a href="/authors/{{ $post->author->username }}"
                     class="hover:underline text-base text-gray-500"">{{ $post->author->name }}</a> in
                 <a href="/categories/{{ $post->category->slug }}"
-                    class="hover:underline text-base text-gray-500">={{ $post->category->name }}</a> |
+                    class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a> |
                 {{ $post->created_at->diffForHumans() }}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
